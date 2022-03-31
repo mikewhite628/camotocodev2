5d1c53c095310a67ad0f2d2cd3b4262e9fbeac67
michael <mikewhite628@gmail.com>
2022-03-30 23:30:28 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import construction from "../src/images/construction.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="App h-full">
+      <nav class="h-12 bg-blue-400 flex relative">
+        <ul class="hidden flex-row justify-evenly w-1/2 absolute items-center right-0 h-full">
+          <li>link1</li>
+          <li>link1</li>
+          <li>link1</li>
+        </ul>
+      </nav>
+      <div className="m-auto px-6">
+        <h1 className="m-auto w-fit my-6">🛠 Under Construction 🛠</h1>
+        <header className=" flex justify-center items-center">
+          <img
+            src={construction}
+            alt="workers"
+            className="md:w-1/2 md:h-1/2 w-full"
+          />
+        </header>
+        <p className="m-auto w-fit my-6 text-center">
+          Camo To Code is evolving into something new! 😎
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
